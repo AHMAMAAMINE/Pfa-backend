e1175c05b8ad436152ec9d382ff80dbbd22b505e
AHMAMAAMINE <74349923+AHMAMAAMINE@users.noreply.github.com>
2022-05-11 01:06:18 +0100

    Update EmailConstant.java

--- src/main/java/com/supportportal/constant/EmailConstant.java
+++ src/main/java/com/supportportal/constant/EmailConstant.java
@@ -3,7 +3,7 @@ package com.supportportal.constant;
 public class EmailConstant {
     public static final String SIMPLE_MAIL_TRANSFER_PROTOCOL = "smtps";
     public static final String USERNAME = "SirIHmprojet@gmail.com";
-    public static final String PASSWORD = "AndyDiamondismamamia1998@";
+    public static final String PASSWORD = "";
     public static final String FROM_EMAIL = "support@getarrays.com";
     public static final String CC_EMAIL = "";
     public static final String EMAIL_SUBJECT = "Get Arrays, LLC - New Password";
